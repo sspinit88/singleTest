7f339bd3fb7d1f2d962ab7d8a2fb0d28b759a316
Sergey Pashnin <s.pashnin@encry.ru>
2018-12-04 10:46:46 +0300

    fin records

--- src/app/system/records/add-event/add-event.component.ts
+++ src/app/system/records/add-event/add-event.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {mergeMap} from 'rxjs/operators';
 
 import * as moment from 'moment';
 
@@ -8,6 +9,7 @@ import {EventsService} from '../../shared/services/events.service';
 import {AEvent} from '../../../shared/models/event.model';
 import {Bill} from '../../../shared/models/bill.model';
 import {BillService} from '../../shared/services/bill.service';
+import {Message} from '../../../shared/models/message.model';
 
 @Component({
     selector: 'app-add-event',
@@ -17,6 +19,8 @@ import {BillService} from '../../shared/services/bill.service';
 export class AddEventComponent implements OnInit {
 
     @Input() categories: Category[] = [];
+
+    message: Message;
 
     private types = [
         {
@@ -36,7 +40,15 @@ export class AddEventComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.message = new Message('danger', '');
+    }
 
+    private showMessage(type: string, text: string) {
+        this.message = new Message(type, text);
+
+        window.setTimeout(() => {
+            this.message.text = '';
+        }, 5000);
     }
 
     onSubmit(form: NgForm) {
@@ -61,9 +73,44 @@ export class AddEventComponent implements OnInit {
             description
         );
 
-        this.eventsService.addEvent(event)
-            .subscribe((bill: Bill) => {
 
+        // получаем денежный счет
+        this.billService.getBill()
+            .subscribe((bill: Bill) => {
+                let value = 0;
+                // если был расход, то надо проверить, хватает ли средств для совершения события
+                if (type === 'outcome') {
+                    if (amount > bill.value) {
+                        // нельзя потратить больше, чем есть на счете
+                        this.message = new Message('danger', `Недостаточно средств! Вам не хватает: ${amount - bill.value}`);
+                        return;
+                    } else {
+                        // вычитаем из счета величину затрат
+                        value = bill.value - amount;
+                    }
+                } else {
+                    // если доход, то складываем значения
+                    value = bill.value + amount;
+                }
+                // в результате в/у действий получаем счет, который нужно обновить
+                // создаем новый метод по обновлению счета в bill.service.ts
+                this.billService.updateBill({value: value, currency: bill.currency})
+                // нужно добавить в подписку this.eventsService.addEvent(event)
+                // для того, что бы не писать два раза .subscribe() воспользуемся
+                // методом margeMap
+                    .pipe(
+                        mergeMap(() => this.eventsService.addEvent(event))
+                    )
+                    .subscribe(() => {
+                        // теперь нужно обнулить значения в форме, вместо метода .reset() воспользуемся setValue(), в нем укажем
+                        // значения нужных нам полей.
+                        form.setValue({
+                            amount: 0,
+                            description: ' ',
+                            category: 1,
+                            type: 'outcome'
+                        });
+                    });
             });
     }
 }
--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
+import {Bill} from '../../../shared/models/bill.model';
+
 @Injectable()
 
 export class BillService {
@@ -13,6 +15,15 @@ export class BillService {
 
     getBill(): Observable<any> {
         return this.http.get('http://localhost:3004/bill')
+            .pipe(
+                map((response) => {
+                    return response;
+                })
+            );
+    }
+
+    updateBill(bill: Bill): Observable<any> {
+        return this.http.put('http://localhost:3004/bill', bill)
             .pipe(
                 map((response) => {
                     return response;
